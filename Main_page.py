8c241260766d5def6b2df95ac6043e64bdb3450c
AnKiTu03 <109533077+AnKiTu03@users.noreply.github.com>
2024-05-17 22:41:21 +0530

    Update Main_page.py

--- Main_page.py
+++ Main_page.py
@@ -13,13 +13,16 @@ import boto3
 from dotenv import load_dotenv
 from io import BytesIO
 import os
+from streamlit_elements import elements, mui, dashboard
+
+st.set_page_config(layout="wide")
 
 load_dotenv()
 access_id = os.getenv('AWS_ACCESS_KEY_ID')
 secret_id = os.getenv('AWS_SECRET_ACCESS_KEY')
 region_name = os.getenv('AWS_REGION')
 
-@st.cache_data(show_spinner=Truepipreqs)
+@st.cache_data(show_spinner=True)
 def load_data_from_s3(bucket_name, file_key):
     session = boto3.Session(
         aws_access_key_id=access_id,
@@ -39,6 +42,36 @@ with st.sidebar:
         "icon": {"font-size": "24px"}, 
         "nav-link": {"font-size": "20px", "text-align": "left", "margin":"0px",  "--hover-color": "#48A6EE" , "margin-top" : "10px"},
         "nav-link-selected": {"background-color": "#48A6EE" , "font-weight" : "100"}})
+
+def Dashboard():
+    bucket_name = 'new-trail01'
+    file_key = 'AccusedData.csv'
+    
+    df = load_data_from_s3(bucket_name, file_key)
+
+    df_filtered_age = df[(df['age'] > 16) & (df['age'] < 60)]
+    crime_pivot = pd.pivot_table(df_filtered_age, values='FIRNo', index='District_Name', columns='age', aggfunc='count', fill_value=0)
+    fig_heatmap = px.imshow(crime_pivot,
+                            labels=dict(x="Age", y="District", color="Crime Count"),
+                            x=crime_pivot.columns,
+                            y=crime_pivot.index,
+                            title='Crime Incidents by District and Age',
+                            color_continuous_scale='viridis')
+    fig_heatmap.update_layout(height=600, width=900)
+
+    city_profession_counts = df.groupby(['District_Name', 'Profession']).size().reset_index(name='Count')
+    fig_city_profession = px.bar(city_profession_counts, x='District_Name', y='Count', color='Profession',
+                                 title='City wise Profession Distribution',
+                                 labels={'District_Name': 'City', 'Count': 'Occurrences', 'Profession': 'Profession'},
+                                 height=500, width=500)
+    fig_city_profession.update_layout(showlegend=False)
+
+    city_caste_counts = df.groupby(['District_Name', 'Caste']).size().reset_index(name='Count')
+    fig_city_caste = px.bar(city_caste_counts, x='District_Name', y='Count', color='Caste',
+                            title='City wise Caste Distribution',
+                            labels={'District_Name': 'City', 'Count': 'Occurrences', 'Caste': 'Caste'},
+                            height=500, width=500)
+    fig_city_caste.update_layout(showlegend=False)
 
 
 if selected == 'DashBoard':
@@ -61,49 +94,8 @@ if selected == 'DashBoard':
         ui.card(title="Cases Pending", content="+3,234", description="+19% from lastmonth",key="card3").render  ()
     with cols[3]:
         ui.card(title="Active Cases    ", content="+2350", description="+18.1% fromlast month", key="card4").render()
+    Dashboard()
 
-    bucket_name = 'new-trail01'
-    file_key = 'AccusedData.csv'
-    
-    df = load_data_from_s3(bucket_name, file_key)
-
-    # Display the heatmap for crime incidents by district and age
-    st.header("Heatmap for Crime Incidents by District and Age")
-    df_filtered_age = df[(df['age'] > 16) & (df['age'] < 60)]
-    crime_pivot = pd.pivot_table(df_filtered_age, values='FIRNo', index='District_Name', columns='age',     aggfunc='count', fill_value=0)
-    fig_heatmap = px.imshow(crime_pivot,
-                            labels=dict(x="Age", y="District", color="Crime Count"),
-                            x=crime_pivot.columns,
-                            y=crime_pivot.index,
-                            title='Crime Incidents by District and Age',
-                            color_continuous_scale='viridis')
-    fig_heatmap.update_layout(height=600, width=900)
-    st.plotly_chart(fig_heatmap, use_container_width=True)
-
-    # Create columns for side-by-side display
-    col1, col2 = st.columns(2)
-
-    with col1:
-        # Display the profession distribution by city
-        st.subheader("City wise Profession Distribution")
-        city_profession_counts = df.groupby(['District_Name', 'Profession']).size().reset_index (name='Count')
-        fig_city_profession = px.bar(city_profession_counts, x='District_Name', y='Count',  color='Profession',
-                                     title='City wise Profession Distribution',
-                                     labels={'District_Name': 'City', 'Count': 'Occurrences',   'Profession': 'Profession'},
-                                     height=600, width=450)
-        fig_city_profession.update_layout(showlegend=False)
-        st.plotly_chart(fig_city_profession, use_container_width=True)
-
-    with col2:
-        # Display the caste distribution by city
-        st.subheader("City wise Caste Distribution")
-        city_caste_counts = df.groupby(['District_Name', 'Caste']).size().reset_index(name='Count')
-        fig_city_caste = px.bar(city_caste_counts, x='District_Name', y='Count', color='Caste',
-                                title='City wise Caste Distribution',
-                                labels={'District_Name': 'City', 'Count': 'Occurrences', 'Caste': 'Caste'},
-                                height=600, width=450)
-        fig_city_caste.update_layout(showlegend=False)
-        st.plotly_chart(fig_city_caste, use_container_width=True)
 
 elif selected == 'MapView':
     patrolling_main()
